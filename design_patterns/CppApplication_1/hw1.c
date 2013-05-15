8372232fa27ecab4e77979dcf5b87fa474386b2a
ugur <ugurdonmez87@gmail.com>
2013-05-15 17:34:51 +0300

    Sync

---
+++ design_patterns/CppApplication_1/hw1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    char number1[80], number2[80];
+    
+    printf("Please enter the first number:");
+    
+    
+    printf("ugur \n");
+    
+    return 0;
+    
+    
+}
