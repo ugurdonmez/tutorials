2d2202b5e6cd8c0647a4c4a0f87b669edb489663
Ugur Donmez <ugurdonmez87@gmail.com>
2014-02-04 11:35:29 +0200

    Two dimensional array merge

---
+++ careercup_solutions/src/com/ugurdonmez/careercup/array/twodimmerge/TwoDimensionMerge.java
@@ -0,0 +1,55 @@
+/*
+ * given 2 Dimensional array
+ * I/P -- String[][] input = { { "abc", "def", "gh" },
+ * { "f", "g" },
+ * { "qrt","xyz","pqr" } };
+ * 
+ * Program shd return a 2-D Array with
+ * O/P -- { { "abcfqrt", "abcfxyz", "abcfpqr" ,abcgqrt and so on ..
+ * 
+ */
+package com.ugurdonmez.careercup.array.twodimmerge;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author uğur
+ */
+public class TwoDimensionMerge {
+
+    public static void main(String asd[]) {
+
+        System.out.println("selin");
+
+        String[][] input = {{"abc", "def", "gh"},
+            {"f", "g"},
+            {"qrt", "xyz", "pqr"},
+            {"ugur","1"}};        
+        
+        for (int i = 0 ; i < input.length-1 ; i++ ) {
+            input[i+1] = merge(input[i], input[i+1]);
+        }
+        
+        for ( int i = 0 ; i < input[input.length-1].length ; i++ ) {
+            System.out.println(input[input.length-1][i]);
+        }
+
+    }
+    
+    public static String [] merge (String [] input1, String [] input2) {
+        
+        String [] output = new String[input1.length * input2.length];
+        
+        int i = 0;
+        
+        for (int j = 0 ; j < input1.length ; j++ ) {
+            for (int k = 0 ; k < input2.length ; k++ ) {
+                output[i++] = input1[j] + input2[k];
+            }
+        }
+        
+        return output;
+        
+    } 
+}
