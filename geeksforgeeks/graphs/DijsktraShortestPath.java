1c9afdf90cf833100fa4205fc32c79dd18718a11
Ugur Donmez <ugur.donmez@unibz.it>
2016-03-09 14:53:14 +0100

    shortest path

--- geeksforgeeks/graphs/DijsktraShortestPath.java
+++ geeksforgeeks/graphs/DijsktraShortestPath.java
@@ -5,10 +5,79 @@
  */
 package graphs;
 
+import java.util.PriorityQueue;
+
 /**
  *
  * @author user
  */
 public class DijsktraShortestPath {
+    int [][] adjv;
+    int size;
     
+    public DijsktraShortestPath(int size) {
+        this.size = size;
+    }
+    
+    public int shortestPath(int start, int end) {
+        
+        PriorityQueue<QueueNode> queue = new PriorityQueue<>();
+        
+        boolean [] visited = new boolean[this.size];
+        
+        QueueNode startNode = new QueueNode();
+        startNode.number = start;
+        startNode.value = 0;
+        
+        queue.add(startNode);
+        
+        while ( !queue.isEmpty() ) {
+            QueueNode node = queue.poll();
+            
+            if (node.number == end) {
+                return node.value;
+            }
+            
+            if (visited[node.number]) {   
+                continue;
+            }
+            
+            visited[node.number] = true;
+            
+            // add adj to queue
+            for (int i = 0; i < size ; i++) {
+                if ( this.adjv[node.number][i] != 0 ) {
+                    QueueNode add = new QueueNode();
+                    add.number = i;
+                    add.value = this.adjv[node.number][i] + node.value;
+                    queue.add(add);
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+    public static void main (String [] args) {
+        
+        DijsktraShortestPath dsp = new DijsktraShortestPath(4);
+        
+        dsp.adjv = new int[][] {{0,1,1,7}, {0,0,0,5}, {0,0,0,1}, {0,0,0,0}};
+        
+        System.out.println( dsp.shortestPath(0, 3) );
+        
+    }
+}
+
+
+class QueueNode implements Comparable<QueueNode>{
+    
+    int value;
+    int number;
+
+    @Override
+    public int compareTo(QueueNode o) {
+        return Integer.compare(this.value, o.value);
+    }
 }
